cc830835167b6a57b2d98c87d302cb6467a2a815
liangwen <liangwen@c3ee6452-1ce1-48d4-b432-aeeb98b5e84e>
2018-07-24 10:21:11 +0000

    7-23订单信息页面

---
+++ taojiamao/ios/taojiamao/taojiamao/Mine/Views/我的订单/订单信息/TJOredrInfoCell.h
@@ -0,0 +1,13 @@
+//
+//  TJOredrInfoCell.h
+//  taojiamao
+//
+//  Created by yueyu on 2018/7/24.
+//  Copyright © 2018年 yueyu. All rights reserved.
+//
+
+#import "TJBaseTableCell.h"
+
+@interface TJOredrInfoCell : TJBaseTableCell
+
+@end
---
+++ taojiamao/ios/taojiamao/taojiamao/Mine/Views/我的订单/订单信息/TJOredrInfoCell.m
@@ -0,0 +1,24 @@
+//
+//  TJOredrInfoCell.m
+//  taojiamao
+//
+//  Created by yueyu on 2018/7/24.
+//  Copyright © 2018年 yueyu. All rights reserved.
+//
+
+#import "TJOredrInfoCell.h"
+
+@implementation TJOredrInfoCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
