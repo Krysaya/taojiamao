bdf4d9965c8a711b1edbf9a80c7d15d9d1471e3e
liangwen <liangwen@c3ee6452-1ce1-48d4-b432-aeeb98b5e84e>
2018-07-25 10:57:55 +0000

    7-25

---
+++ taojiamao/ios/taojiamao/taojiamao/TQGou/Models/TJTqgGoodsModel.h
@@ -0,0 +1,16 @@
+//
+//  TJTqgGoodsModel.h
+//  taojiamao
+//
+//  Created by yueyu on 2018/7/25.
+//  Copyright © 2018年 yueyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TJTqgGoodsModel : NSObject
+
+@property(nonatomic,strong)NSNumber *total_results;
+//@property (nonatomic, strong) NSString *results.title;
+//@property (nonatomic, strong) NSString *results.total_amount;
+@end
---
+++ taojiamao/ios/taojiamao/taojiamao/TQGou/Models/TJTqgGoodsModel.m
@@ -0,0 +1,13 @@
+//
+//  TJTqgGoodsModel.m
+//  taojiamao
+//
+//  Created by yueyu on 2018/7/25.
+//  Copyright © 2018年 yueyu. All rights reserved.
+//
+
+#import "TJTqgGoodsModel.h"
+
+@implementation TJTqgGoodsModel
+
+@end
