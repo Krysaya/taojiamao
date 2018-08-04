0d373d3a8e52ca177d7c3c357e5ff4b1c7b9ef52
liangwen <liangwen@c3ee6452-1ce1-48d4-b432-aeeb98b5e84e>
2018-08-04 10:38:50 +0000

    8-4第三方 qiandao jiekou

---
+++ taojiamao/ios/taojiamao/taojiamao/Home/Models/头条/评论/TJCommentsListModel.h
@@ -0,0 +1,23 @@
+//
+//  TJCommentsListModel.h
+//  taojiamao
+//
+//  Created by yueyu on 2018/8/4.
+//  Copyright © 2018年 yueyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TJCommentsListModel : NSObject
+
+@property (nonatomic, strong) NSString *addtime;
+@property (nonatomic, strong) NSString *aid;
+@property (nonatomic, strong) NSString *content;
+@property (nonatomic, strong) NSString *id;
+@property (nonatomic, strong) NSString *image;
+@property (nonatomic, strong) NSString *num;
+@property (nonatomic, strong) NSString *pid;
+@property (nonatomic, strong) NSString *uid;
+@property (nonatomic, strong) NSString *username;
+
+@end
---
+++ taojiamao/ios/taojiamao/taojiamao/Home/Models/头条/评论/TJCommentsListModel.m
@@ -0,0 +1,14 @@
+
+//
+//  TJCommentsListModel.m
+//  taojiamao
+//
+//  Created by yueyu on 2018/8/4.
+//  Copyright © 2018年 yueyu. All rights reserved.
+//
+
+#import "TJCommentsListModel.h"
+
+@implementation TJCommentsListModel
+
+@end
